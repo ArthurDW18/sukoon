b9b6f14e9ca67aca94129de6115bf5213fc8fd45
Z0mb1e <adwfaculdade@outlook.com>
2026-06-18 23:23:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
 	greetingNight: 'Boa noite,',
 
 	// Layout
-	layout: 'buttons', // 'bento', 'lists', 'buttons'
+	layout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'f23324aebe1f463075ae77510ab48727', // Get your API key from https://openweathermap.org/
